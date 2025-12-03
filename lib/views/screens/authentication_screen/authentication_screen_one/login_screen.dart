933e4ca5c3e00dddbb7424833064c389bdc171ba
linconchondroroy <linconchondroroy@gmail.com>
2025-12-03 17:09:02 +0600

    New Update

--- lib/views/screens/authentication_screen/authentication_screen_one/login_screen.dart
+++ lib/views/screens/authentication_screen/authentication_screen_one/login_screen.dart
@@ -11,36 +11,45 @@ class LoginScreen extends GetView<LoginScreenController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.primaryColor,
-      appBar: AppBar(
         backgroundColor: AppColors.primaryColor,
-      ),
-      body: Column(
-        children: [
-          SizedBox(height: 226.1.h,),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(AppImages.login_screen),
-            ],
-          ),
+        appBar: AppBar(
+          backgroundColor: AppColors.primaryColor,
+        ),
+        body: Column(
+          children: [
+            SizedBox(height: 226.1.h,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(AppImages.login_screen),
+              ],
+            ),
 
-          SizedBox(height: 30.h,),
-
-          Column(
-            children: [
-              TextField(
-                decoration: InputDecoration(
-                  hintText: "example@gmail.com",
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8),
+            SizedBox(height: 30.h,),
+            
+            Padding(padding: EdgeInsets.symmetric(horizontal: 16.w),
+              child: SizedBox(
+                width: 398.w,
+                height: 54.h,
+                child: TextFormField(
+                  style: TextStyle(color: AppColors.textPrimaryColor),
+                  decoration: InputDecoration(
+                    hintText: "example@gmail.com",
+                    hintStyle: TextStyle(
+                      color: AppColors.textPrimaryColor,),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.r),
+                      borderSide: BorderSide(
+                        color: AppColors.textPrimaryColor,
+                        width: 1.5,
+                      )
+                      )
+                    )
                   ),
                 ),
               ),
-            ],
-          ),
-        ],
-      )
+          ],
+        )
     );
   }
 }
